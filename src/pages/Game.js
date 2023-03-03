a75a1468cc184562ab93d73cd45f6ee6afaf9e26
ZhekLu <justletmego@list.ru>
2023-03-03 19:54:20 +0100

    Add base styles and Game component structure

---
+++ src/pages/Game.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Game() {
+  return (
+    <section className='container'>
+      <section className='gallery col-lg-9 col-md-9 col-sm-12 col-xs-12'>
+                Gallery
+      </section>
+      <section className='panel col-lg-3 col-md-3 col-sm-12 col-xs-12'>
+                Panel
+      </section>
+    </section>
+  );
+}
