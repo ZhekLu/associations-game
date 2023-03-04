78f9acb696f9e651bd725ab0baafe575bc8d563a
ZhekLu <justletmego@list.ru>
2023-03-04 11:12:42 +0100

    Add 3d effects

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import picture from '../images/rose-grass-400x400.jpg';
+import selectIcon from '../images/check.png';
+import removeIcon from '../images/cross.png';
 
 export default function GameCard() {
-  return (
-    <article className='game-card col-lg-2 col-md-2 col-sm-2 col-xs-5'>
-      <img className='card-image' src={picture} alt='card'/>
-      <h4 className='card-content'>Content</h4>
-    </article>
-  );
+  return (<article className='game-card col-lg-2 col-md-2 col-sm-2 col-xs-5'>
+    <div className='card-cover'>
+      <div className='card-front'>
+        <img className='card-image' src={picture} alt='card'/>
+        <h4 className='card-content'>Content</h4>
+      </div>
+      <div className='card-back'>
+        <button className='icon-button'>
+          <img src={removeIcon} alt='Remove card'/>
+        </button>
+        <button className='icon-button'>
+          <img src={selectIcon} alt='Select card'/>
+        </button>
+      </div>
+    </div>
+  </article>);
 }
