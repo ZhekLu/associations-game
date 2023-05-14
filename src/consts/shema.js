432e88c818fce3c4eae47b6aabfb9c940e00456f
ZhekLu <justletmego@list.ru>
2023-05-14 16:33:21 +0300

    feat: Add menu logic

---
+++ src/consts/shema.js
@@ -0,0 +1,5 @@
+export default class GameID {
+  static minLength = 4;
+  static maxLength = 4;
+  static pattern = '\\w{4}';
+}
--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,24 +1,115 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
 import './Menu.css';
+import GameID from '../consts/shema';
+import getGameSets from '../google/drive/set';
+
+const SelectSet = ({onSetSelection}) => {
+  const [gameSets, setGameSets] = useState([]);
+  useEffect(() => {
+    getGameSets().then((data) => {
+      setGameSets(data);
+    });
+  }, []);
+  return (
+    <>
+      <h1>Select Set</h1>
+      <div className='select-set'>
+        {
+          gameSets.map((set) => (
+            <button
+              value={set.id}
+              className='select-set-button'
+              onClick={() => onSetSelection(set)}
+              key={set.id}
+            >{set.name}
+            </button>
+          ))
+        }
+      </div>
+    </>
+  );
+};
+
+SelectSet.propTypes = {
+  onSetSelection: PropTypes.func.isRequired,
+};
+
+const SelectOption = ({
+  handleNewGameClick,
+  handleJoinGameClick,
+  handleGameIDChange,
+}) => {
+  return (<>
+    <h1>Select option</h1>
+    <button
+      className='menu-button'
+      onClick={handleNewGameClick}
+    > New Game
+    </button>
+    <h1>Or</h1>
+    <button
+      className='menu-button'
+      onClick={handleJoinGameClick}
+      disabled={true}
+    > Join Game
+    </button>
+    <input
+      type='text'
+      onChange={handleGameIDChange}
+      placeholder='Game ID'
+      pattern={GameID.pattern}
+      minLength={GameID.minLength}
+      maxLength={GameID.maxLength}
+    />
+  </>);
+};
+
+SelectOption.propTypes = {
+  handleNewGameClick: PropTypes.func.isRequired,
+  handleJoinGameClick: PropTypes.func.isRequired,
+  handleGameIDChange: PropTypes.func.isRequired,
+};
 
 export default function Menu() {
+  const [isNewGame, setIsNewGame] = useState(undefined);
+  const [gameID, setGameID] = useState(undefined);
+  const [gameSet, setGameSet] = useState(undefined);
+
+  const handleGameIDChange = (event) => {
+    const validityState = event.target.validity;
+    const isOnlyPatternInvalid = validityState.patternMismatch &&
+            !validityState.tooLong &&
+            !validityState.tooShort;
+    event.target.setCustomValidity(isOnlyPatternInvalid ?
+            'Game ID should contains only digits or letters' :
+            '',
+    );
+    if (validityState.valid) {
+      setGameID(event.target.value);
+    }
+  };
+  const handleNewGameClick = (event) => {
+    setIsNewGame(true);
+  };
+  const handleJoinGameClick = (event) => {
+    setIsNewGame(false);
+  };
+
+  const handleGameSetSelection = (selectedSet) => {
+    setGameSet(selectedSet);
+  };
+
   return (
     <div className='overlay'>
       <section className='menu-body col-lg-4 col-md-4 col-sm-6 col-xs-12'>
-        <h1>Select option</h1>
-        <button className='menu-button'>New Game</button>
-        <button className='menu-button'>Join Game</button>
-        <input type='text' required/>
-        <h1>Select Set</h1>
-        <div className='select-set'>
-          <button className='select-set-button'>Set 1</button>
-          <button className='select-set-button'>Set 2</button>
-          <button className='select-set-button'>Set 3</button>
-          <button className='select-set-button'>Set 4</button>
-          <button className='select-set-button'>Set 5</button>
-          <button className='select-set-button'>Set 6</button>
-          <button className='select-set-button'>Set 7</button>
-        </div>
+        {isNewGame ?
+                    <SelectSet onSetSelection={handleGameSetSelection}/> :
+                    <SelectOption
+                      handleGameIDChange={handleGameIDChange}
+                      handleNewGameClick={handleNewGameClick}
+                      handleJoinGameClick={handleJoinGameClick}
+                    />}
       </section>
     </div>
   );
