ab4be60d73b23cec4d4dfdbe5ac2cf7ee433cea7
ZhekLu <justletmego@list.ru>
2023-05-18 21:21:53 +0300

    Add copy game id to clipboard button

--- src/components/GamePanel.js
+++ src/components/GamePanel.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Logo from './Logo';
 import notSelectedPicture from '../images/unknown.jpg';
 import copyIcon from '../images/copy-icon.png';
 import selectIcon from '../images/select-icon.png';
+import {GameContext} from '../context/GameContext';
 
 export default function GamePanel() {
+  const {gameID} = useContext(GameContext);
+
   return (
     <nav className='panel col-lg-3 col-md-3 col-sm-12 col-xs-12'>
       <Logo/>
@@ -18,18 +21,23 @@ export default function GamePanel() {
         />
         <h4 className='card-content'>Content</h4>
       </article>
+
       {/* Control buttons*/}
       <section className='panel-buttons col-lg-6 col-md-6 col-sm-3 col-xs-3'>
-        <button className='icon-button'>
+        <button className='icon-button' onClick={() => {
+          navigator.clipboard.writeText(gameID)
+              .then(() => console.log('Copied!'));
+        }}>
           <img src={copyIcon} alt='copy'/>
         </button>
         <button className='icon-button'>
           <img src={selectIcon} alt='select'/>
         </button>
       </section>
+
       {/* Game code*/}
       <section className='game-code col-lg-6 col-md-6 col-sm-3 col-xs-3'>
-        <code>code</code>
+        <code>{gameID}</code>
       </section>
     </nav>
   );
