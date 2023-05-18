7f9832cee7d0f513b96ba3c36cf7a25565b50d06
ZhekLu <justletmego@list.ru>
2023-05-18 21:18:16 +0300

    Add gameset with dynamicly loading of cards

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,15 +1,36 @@
-import React from 'react';
-import picture from '../images/rose-grass-400x400.jpg';
+import React, {useEffect, useState} from 'react';
+import loadingImage from '../images/loading.jpg';
 import selectIcon from '../images/check-icon.png';
 import crossIcon from '../images/cross-icon.png';
 import removePlaceholder from '../images/cross-placeholder.png';
+import PropTypes from 'prop-types';
 
-export default function GameCard() {
+export default function GameCard({gameID, setID, cardID, selectCard}) {
+  const [image, setImage] = useState('');
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchImage = async () => {
+      try {
+        const response = await import(`../gamesets/${setID}/img_${cardID}.jpg`);
+        setImage(response.default);
+      } catch (err) {
+        console.log('Error while loading picture:', err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchImage();
+  }, []);
+
   return (
     <article className='game-card col-lg-2 col-md-2 col-sm-2 col-xs-5'>
       <div className='card-cover'>
         <div className='card-front'>
-          <img className='card-image' src={picture} alt='card'/>
+          <img
+            className='card-image'
+            src={loading ? loadingImage : image}
+            alt='card'
+          />
           <img className='card-image-placeholder'
             src={removePlaceholder} alt='cross'
           />
@@ -19,7 +40,7 @@ export default function GameCard() {
           <button className='icon-button'>
             <img src={crossIcon} alt='Remove card'/>
           </button>
-          <button className='icon-button'>
+          <button className='icon-button' onClick={() => selectCard(cardID)}>
             <img src={selectIcon} alt='Select card'/>
           </button>
         </div>
@@ -27,3 +48,10 @@ export default function GameCard() {
     </article>
   );
 }
+
+GameCard.propTypes = {
+  gameID: PropTypes.string.isRequired,
+  setID: PropTypes.number.isRequired,
+  cardID: PropTypes.number.isRequired,
+  selectCard: PropTypes.func.isRequired,
+};
