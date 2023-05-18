33f63a6b2364714593d42a3d1dc9c44ea3cde63d
ZhekLu <justletmego@list.ru>
2023-05-18 16:24:53 +0300

    Add libs loading status to auth context

--- src/google/auth/AuthContext.js
+++ src/google/auth/AuthContext.js
@@ -7,15 +7,24 @@ export const AuthContext = createContext({
   setProfile: undefined,
   client: undefined,
   setClient: undefined,
+  googleLibsLoaded: {gis: undefined, gapi: undefined},
+  setGoogleLibsLoaded: undefined,
 });
 
 export const AuthProvider = ({children}) => {
   const [accessToken, setAccessToken] = useState(null);
   const [profile, setProfile] = useState(null);
   const [client, setClient] = useState(null);
+  const [googleLibsLoaded, setGoogleLibsLoaded] = useState({
+    gis: false,
+    gapi: false,
+  });
 
   const contextValue = {
-    accessToken, setAccessToken, profile, setProfile, client, setClient,
+    accessToken, setAccessToken,
+    profile, setProfile,
+    client, setClient,
+    googleLibsLoaded, setGoogleLibsLoaded,
   };
 
   return <AuthContext.Provider value={contextValue}>
