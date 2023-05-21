08b3d2d5950cbad416d9203abbcb009a26703558
ZhekLu <justletmego@list.ru>
2023-05-21 22:50:32 +0300

    Fix back to menu button

--- src/components/GameResult.js
+++ src/components/GameResult.js
@@ -1,19 +1,15 @@
-import React, {useState} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import winImage from '../images/success-icon.png';
 import loseImage from '../images/lose-icon.png';
-import {CleanContexts} from '../context/manager';
+import {backToMenu} from '../services/navigation';
 
 export default function GameResult({isWin}) {
-  const [backToMenu, setBackToMenu] = useState(false);
   const header = isWin ? 'Great!' : 'Ooops';
   const content = isWin ?
         'It seems that you easily guess people\'s thoughts' :
         'You don\'t guess, but you\'ll definitely make it next time';
-  if (backToMenu) {
-    return <CleanContexts/>;
-  }
   return (
     <section
       className='gallery game-result col-lg-9 col-md-9 col-sm-12 col-xs-12'
@@ -27,7 +23,9 @@ export default function GameResult({isWin}) {
         <p className='menu-body-text'>{content}</p>
         <button
           className='menu-button'
-          onClick={() => setBackToMenu(true)}
+          onClick={() => {
+            backToMenu();
+          }}
         > Back to menu
         </button>
       </section>
---
+++ src/services/navigation.js
@@ -0,0 +1,3 @@
+export function backToMenu() {
+  window.location.reload();
+}
