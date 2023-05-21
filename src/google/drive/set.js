c3f700db00549b90decc185d671340e4cdcc92b2
ZhekLu <justletmego@list.ru>
2023-05-21 23:06:43 +0300

    Add new gamesets

--- src/google/drive/set.js
+++ src/google/drive/set.js
@@ -2,6 +2,8 @@ export async function getGameSets() {
   return [
     {'id': 1, 'name': 'Test green set'},
     {'id': 2, 'name': 'Unknown set'},
+    {'id': 3, 'name': 'Karazina Life'},
+    {'id': 4, 'name': 'Disney'},
   ];
 }
 
@@ -9,5 +11,7 @@ export default function getGameSetsDescription() {
   return {
     1: [],
     2: [],
+    3: [],
+    4: [],
   };
 }
